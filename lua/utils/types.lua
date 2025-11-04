379541e3d8d5c10f605b466546d09e846d111145
Adam Schubach <33351047+TheAtomicShoebox@users.noreply.github.com>
2025-11-04 11:32:59 -0500

    update types

--- lua/utils/types.lua
+++ lua/utils/types.lua
@@ -8,16 +8,16 @@ local M = {}
 ---@field max_depth number
 ---@field indent_step string
 
----@param s string | table
----@return table
-function M.ToTable(s) end
-
----@param msg string
----@param level? snacks.notifier.level|number
----@param opts? snacks.notifier.Notif.opts
-function M.Log(msg, level, opts) end
-
----@param value any?
----@param opts utils.Serialize.Options?
----@return string
-function M.Serialize(value, opts) end
+------@param s string | table
+------@return table
+---function M.ToTable(s) end
+---
+------@param msg string
+------@param level? snacks.notifier.level|number
+------@param opts? snacks.notifier.Notif.opts
+---function M.Log(msg, level, opts) end
+---
+------@param value any?
+------@param opts utils.Serialize.Options?
+------@return string
+---function M.Serialize(value, opts) end
