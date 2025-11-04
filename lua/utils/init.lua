41a15617c15d35cfff68d30692577359878009f5
Adam Schubach <33351047+TheAtomicShoebox@users.noreply.github.com>
2025-11-04 12:04:49 -0500

    Using template now

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -1,8 +1,17 @@
+local debug = require("utils.lib.debug")
+
+---@class utils
 local M = {}
 
 M.reload = function(module)
 	package.loaded[module] = nil
 	return require(module)
 end
+
+M.Serialize = debug.Serialize
+
+M.Log = debug.Log
+
+M.ToTable = debug.ToTable
 
 return M
